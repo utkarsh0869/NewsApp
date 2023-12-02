7883aed957fa682d3d00abf5f40e28ff55c6b1a0
Utkarsh Karki <utkarsh1998@icloud.com>
2023-12-01 16:44:28 -0800

    detail fragment done

--- app/src/main/java/com/example/newsapp/detailnews/DetailNewsFragment.kt
+++ app/src/main/java/com/example/newsapp/detailnews/DetailNewsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.newsapp.R
 import com.example.newsapp.network.models.Articles
@@ -13,11 +14,28 @@ class DetailNewsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_detail_news, container, false)
+
         val args = arguments?.getSerializable("article") as? Articles
 
+        val newsDetailTitle : TextView = view.findViewById(R.id.newsDetailTitle)
+        val newsDetailDate : TextView = view.findViewById(R.id.newsDetailDate)
+        val newsDetailAuthor : TextView = view.findViewById(R.id.newsDetailAuthor)
+        val newsDetailSource : TextView = view.findViewById(R.id.newsDetailSource)
+        val newsDetailDescription : TextView = view.findViewById(R.id.newsDetailDescription)
+        val newsDetailLink : TextView = view.findViewById(R.id.newsDetailLink)
 
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_news, container, false)
+        if (args != null) {
+            newsDetailTitle.text = args.title
+            newsDetailDate.text = args.publishedAt
+            newsDetailAuthor.text = args.author
+            newsDetailSource.text = args.source.name
+            newsDetailDescription.text = args.description
+            newsDetailLink.text = args.url
+        }
+
+
+        return view
     }
 
 }
